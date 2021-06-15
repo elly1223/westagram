f30283927d09b979830ec062df541817a9ffc560
이윤경 <62500947+yunkyung-creator@users.noreply.github.com>
2021-06-15 18:48:47 +0900

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -5,7 +5,21 @@ thisIsButton.addEventListener('click', function() {
   const loginPassword = document.getElementById('password').value;
 
   loginId !== null && loginPassword !== null ? alert("로그인 성공") : alert("로그인 실패");
-  
-});
+
+}
+
+const button = document.getElementsByClassName('login_btn')[0];
+const input = document.getElementsByClassName('input')[0];
+const id = document.getElementById('username')[0];
+const pw = document.getElementById("password")[0];
+
+function changeColor() {
+    if(id.value && pw.value) {
+        button.removeAttribute('disabled');
+        button.style.backgroundColor = '#0095F6';
+    }
+}
+
+input.addEventListener('keyup', changeColor);
 
 //  loginId != null && loginPassword != null ? alert("로그인 성공") : alert("로그인 실패");
