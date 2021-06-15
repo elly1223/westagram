1a8ff8a832dd8079ae51f9b9b53d8e6c9ed52cf2
이윤경 <62500947+yunkyung-creator@users.noreply.github.com>
2021-06-16 03:14:15 +0900

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -1,25 +1,14 @@
-const thisIsButton = document.getElementsByClassName('login_btn')[0];
+const btn = document.getElementById('button');
+const id = document.getElementById('userId');
+const password = document.getElementById('password');
 
-thisIsButton.addEventListener('click', function() {
-  const loginId = document.getElementById('username').value;
-  const loginPassword = document.getElementById('password').value;
+const checkInput = () => {
+  if (id.value.length && password.value.length) {
+    btn.style.backgroundColor = '#4697EF';
+  } else {
+    btn.style.backgroundColor = '#badffa';
+  }
+};
 
-  loginId !== null && loginPassword !== null ? alert("로그인 성공") : alert("로그인 실패");
-
-}
-
-const button = document.getElementsByClassName('login_btn')[0];
-const input = document.getElementsByClassName('input')[0];
-const id = document.getElementById('username')[0];
-const pw = document.getElementById("password")[0];
-
-function changeColor() {
-    if(id.value && pw.value) {
-        button.removeAttribute('disabled');
-        button.style.backgroundColor = '#0095F6';
-    }
-}
-
-input.addEventListener('keyup', changeColor);
-
-//  loginId != null && loginPassword != null ? alert("로그인 성공") : alert("로그인 실패");
+id.addEventListener('keyup', checkInput);
+password.addEventListener('keyup', checkInput);
