6eca900480024316b9a69d2e5dcd13fce8b126a9
이윤경 <62500947+yunkyung-creator@users.noreply.github.com>
2021-06-18 14:47:09 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 const replyButton = document.getElementById('reply-btn');
+const input = document.getElementById('input');
+const list = document.getElementById('list');
 
 replyButton.addEventListener('click', function(e) {
-    if (e.keycode === 13) {
-        console.log('aa');    
-    }
+    const temp = document.createElement('li');
+    temp.innerHTML = input.value;
+    list.appendChild(temp);
 });
